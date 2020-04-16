41beac5d533d65b65de408359739c968d03120ea
Paulo Rezende <paulo@pmbr.net>
2020-04-16 16:04:44 -0700

    first commit

---
+++ src/main/java/logic/MeetingScheduler.java
@@ -0,0 +1,17 @@
+package logic;
+
+import model.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeetingScheduler {
+
+    private List<Room> roomPool = new ArrayList<>();
+
+    public Room scheduleMeeting() {
+
+    }
+
+
+}
